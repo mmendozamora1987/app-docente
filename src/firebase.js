72a547cd285e0b39e01231126d8b8ffadadb302f
mmendozamora1987 <miguelmendoza@ielleras.edu.co>
2026-05-05 11:49:51 -0500

    Delete src directory

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD2qNHy5V5uygaHp1dRf7n05CFItm3KOFs",
-  authDomain: "app-docente-profesional.firebaseapp.com",
-  projectId: "app-docente-profesional",
-  storageBucket: "app-docente-profesional.firebasestorage.app",
-  messagingSenderId: "778220930573",
-  appId: "1:778220930573:web:610178faaffc5d65797744"
-};
